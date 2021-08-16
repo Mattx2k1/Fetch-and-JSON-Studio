a93571feb6f803f4d1db8695f84290bed6e5734e
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-08-16 19:52:34 -0400

    up part 2 of requirements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        return response.json();
+
+    })
+        .then()
+})
